2128ed42e3471aa5ca41e05f4749c2f8b5547c25
jay bay <gratholio@gmail.com>
2014-11-09 11:01:12 -0500

    Queue

--- src/QUEUE.java
+++ src/QUEUE.java
@@ -1,4 +1,28 @@
 
 public class QUEUE {
+	
+	private int mSize;
+	private int[] queueArr;
+	private int enqueue;
+	private int dequeue;
+	private int size;
+	
+	
+	public QUEUE(int inSize){
+		this.mSize = inSize;
+		queueArr = new int[mSize];
+		enqueue = 0;
+		dequeue = 0;
+		size = 0;
+	}
+	
+	public void enqueue(int inInt){
+		queueArr[enqueue] = inInt;enqueue++;
+	}
 
+	public int dequeue(){
+		int returnVal = dequeue;dequeue++;
+		return queueArr[returnVal];
+	}
+	
 }
