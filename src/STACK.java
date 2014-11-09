ec71a6886eb668cdf8e93ac153b4cafbfa8f1f43
jay bay <gratholio@gmail.com>
2014-11-09 10:30:47 -0500

    Stack Implemented

--- src/STACK.java
+++ src/STACK.java
@@ -1,4 +1,41 @@
+//Stack Implementation 
+
 
 public class STACK {
+	
+	private int mSize;
+	private int top;
+	private int[] stackArr;
+	
+	public STACK(int inSize){
+		this.mSize = inSize;
+		stackArr = new int[mSize];
+		this.top = -1;
+	}
+	
+	public void push (int inPush){
+		stackArr[++top] = inPush; //pre-increment operator
+	}
+	
+	public int pop (int inPop){
+		return (stackArr[top--]);//post increment operator
+	}
 
+	public boolean emptyStack(){
+		if (top == -1)
+			return true;
+		else
+			return false;
+	}
+	
+	public int peek(){
+		return stackArr[top];
+	}
+
+	public boolean fullStack(){
+		if (top > -1)
+			return true;
+		else 
+			return false;
+	}
 }
