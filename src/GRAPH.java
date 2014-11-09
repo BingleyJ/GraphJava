ca61617ff0a249d4941133c1fb0d4da58b9dd241
jay bay <gratholio@gmail.com>
2014-11-09 11:30:58 -0500

    GRAPH

--- src/GRAPH.java
+++ src/GRAPH.java
@@ -1,4 +1,16 @@
 
 public class GRAPH {
-
+	private VERTEX[] vertexArr;
+	private int[][] matrix;
+	private int totalVertices;
+	private STACK stack;
+	private QUEUE queue;
+	private int size;
+	
+	public GRAPH(int inSize){
+		this.size = inSize;
+		totalVertices = 0;
+		vertexArr = new VERTEX[size];
+		matrix = new int[size][size];
+	}
 }
