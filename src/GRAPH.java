6fc3fa6edfb40925196585e5a3ba201eed9681ec
jay bay <gratholio@gmail.com>
2014-11-09 11:42:06 -0500

    GRAPH

--- src/GRAPH.java
+++ src/GRAPH.java
@@ -12,5 +12,35 @@ public class GRAPH {
 		totalVertices = 0;
 		vertexArr = new VERTEX[size];
 		matrix = new int[size][size];
+		resetMatrix();
 	}
+	
+	public void resetMatrix(){
+		for (int i = 0; i < size; i++){
+			for (int j = 0; j < size; j++){
+				matrix[i][j] = 0;
+	}	}	}
+	
+	public void addVertex(char label){
+		
+	}
+	
+	public void addEdge(int inSource, int inDest){
+		
+	}
+	
+	public void drawvertex(int inVertex){
+		
+	}
+	
+	public void breadthFS(){
+		
+	}
+	
+	public void DepthFS(){
+		
+	}
+	
 }
+
+	
